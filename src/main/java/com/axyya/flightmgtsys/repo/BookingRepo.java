79b171a80316992d53427f6a3a98b19f5f66499f
Mohd Shariq <mdshariq614@gmail.com>
2022-06-18 22:59:56 +0530

    Add controller endpoint tests.

--- src/main/java/com/axyya/flightmgtsys/repo/BookingRepo.java
+++ src/main/java/com/axyya/flightmgtsys/repo/BookingRepo.java
@@ -1,9 +1,10 @@
 package com.axyya.flightmgtsys.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.axyya.flightmgtsys.model.Booking;
-
+@Repository
 public interface BookingRepo extends JpaRepository<Booking, Integer> {
 
 }
